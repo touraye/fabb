ee2e914bc6e01d068d485a965d801ae6cdf20e1a
touraye <touraye07@gmail.com>
2025-08-09 10:31:18 +0000

    create dedicated page for displaying categories listing

---
+++ app/categories/page.tsx
@@ -0,0 +1,13 @@
+import CategoriesList from "@/components/CategoriesList";
+
+export default function CategoryPage() {
+    return (<>
+        {/* Hero section display the most recent post */}
+
+        {/* Tabs for selecting different categories */}
+        {/* from the categories there are: gallery, new, and projects */}
+        {/* Tabs: All, News, Project, Gallery */}
+        {/* Each should pull their respective data*/}
+        <CategoriesList />
+    </>)
+}
